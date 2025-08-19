f389e6051eadb91b37ae0829dae39710f132c893
Wsanbey <welrysanbey@gmail.com>
2025-08-19 03:44:14 -0300

    [dyad] wrote 1 file(s), executed 2 SQL queries

--- src/pages/CourseDetailsPage.tsx
+++ src/pages/CourseDetailsPage.tsx
@@ -69,7 +69,7 @@ const CourseDetailsPage: React.FC = () => {
         id: courseData.id,
         title: courseData.title,
         description: courseData.description,
-        imageUrl: courseData.image_url,
+        image_url: courseData.image_url, // Corrigido de 'imageUrl' para 'image_url'
         lessons: lessonsData || [],
       };
       setCourse(fullCourse);
